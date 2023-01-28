9b386502c9569764419beea42e229a0fddc4866b
José Silva <henrique@hsti.eng.br>
2023-01-28 11:42:15 -0300

    Pratica spring clould

--- src/main/java/com/eldorado/microservico/autenticacao/feign/UserInterface.java
+++ src/main/java/com/eldorado/microservico/autenticacao/feign/UserInterface.java
@@ -3,12 +3,14 @@ package com.eldorado.microservico.autenticacao.feign;
 
 import com.eldorado.commons.dto.UserDto;
 import com.eldorado.commons.dto.UserLoginDto;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "ms-usuario")
+@FeignClient(name = "ms-usuario")
 public interface UserInterface {
 
     @PostMapping(value = "/user/login")
